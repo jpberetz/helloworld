178ae56077704fc4e99de1e59d9170eecafbf841
John Beretz <jpberetz@Lux.local>
2022-04-19 17:29:02 -0700

    version 1 of Hello World

---
+++ hello_world_helper.rb
@@ -0,0 +1,5 @@
+class HelloWorld
+  def say_hello
+    puts "Hello World!"
+  end
+end
---
+++ hello_world_start.rb
@@ -0,0 +1,6 @@
+$LOAD_PATH << '.'
+
+require "hello_world_helper"
+
+helper = HelloWorld.new
+helper.say_hello
